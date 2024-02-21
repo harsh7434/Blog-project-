e45d5bc061760a58b073f2cd1a2fc88fce05c754
Harsh <rathodharsh7434@gmail.com>
2024-02-21 12:19:33 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1/adminPanal2");
+
+const db = mongoose.connection;
+
+db.once("open", function (err) {
+    if (err) console.log(err);
+    console.log("db is connected");
+})
+
+module.exports = db
